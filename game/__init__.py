73f2877a6c95a5a6db8d8b8c77baaa5d4c99898c
e-ji1107 <diddmswl1107@gmail.com>
2025-02-17 17:11:12 +0900

    패키지

---
+++ game/__init__.py
@@ -0,0 +1,7 @@
+from .graphic.render import render_test
+VERSION = 3.6
+
+def print_version_info():
+    print(f"The version of this game is {VERSION}")
+
+print("Initializing Game...") # import 했을 때 한 번만 출력되고 그 이후로는 나오지 않는다.
---
+++ game/graphic/render.py
@@ -0,0 +1,5 @@
+from game.sound.echo import echo_test #명시적으로 다른폴더(사운드)내의 모듈 호출
+
+def render_test():
+    print("render")
+    echo_test()
